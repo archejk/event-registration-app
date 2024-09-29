0784a3fcfb3dec8abca51f458df7cd5a99b83859
archejk <jkarche13@gmail.com>
2024-09-29 23:39:02 +0800

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "selectPhilippinesAddress", to: "https://cdn.jsdelivr.net/npm/select-philippines-address@1.0.6/+esm", preload: true
